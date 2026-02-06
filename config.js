bd8ce5b5d8c12d25d041f1aebf56c9bc955cd0b7
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:46:39 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NARVENT';
+export let nameSongs = [
+	"MEMORY REBOOT (SPED UP)", /*1*/
+	"FAINTED", /*2*/
+	"LOST MEMORY", /*3*/
+	"EUPHORIA", /*4*/
+	"MOONLIT", /*5*/
+	"HER EYES", /*6*/
+	"HAUNTED", /*7*/
+	"PARTIALITY", /*8*/
+	"MEMORY REBOOT", /*9*/
+	"DAYDREAM", /*10*/
+	"LUNAR", /*11*/
+	"HEAR ME", /*12*/
+	"VANISHED", /*13*/
+	"FAKE LOVE", /*14*/
+	"WINTER MEMORIES", /*15*/
+	"SUFFERING", /*16*/
+	"CALM NIGHT", /*17*/
+	"REAL FOR ME", /*18*/
+	"OBSESSED", /*19*/
+	"MEMORY REBOOT (SLOWED)", /*20*/
+]
